978d7c435c8547205d04a6fca32c77f96b96e750
hicaro23 <136822924+hicaro23@users.noreply.github.com>
2024-11-29 20:08:53 -0300

    Add files via upload

---
+++ meu_site.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#cria a 1º pagina do soite
+#route -> nomedosite.com/caminho
+#funcao -> o que voce quer exibir na página
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    #return nome_usuario
+    return render_template("usuarios.html",nome_usuario = nome_usuario)
+
+#colocar site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
