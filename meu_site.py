ceb9a47c2e2160878b58a4e1d8bf621d3699469c
hicaro23 <136822924+hicaro23@users.noreply.github.com>
2024-11-29 20:23:27 -0300

    Add files via upload

--- meu_site.py
+++ meu_site.py
@@ -20,5 +20,5 @@ def usuarios(nome_usuario):
     return render_template("usuarios.html",nome_usuario = nome_usuario)
 
 #colocar site no ar
-if __name__ == "__main__":
+if __name__ == "__master__":
     app.run(debug=True)
